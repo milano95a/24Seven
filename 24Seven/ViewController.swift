20e9271413005c27ebef9b1c76be068e91366b86
abu <milano95j@gmail.com>
2021-02-24 21:26:30 +0500

    Initial Commit

---
+++ 24Seven/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  24Seven
+//
+//  Created by Abdurakhmon Jamoliddinov on 2/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
